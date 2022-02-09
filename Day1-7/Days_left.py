5a667b5a22c9167dc29423d0c8fa2dc78d56d45d
AngadManroy <angadmanroy@gmail.com>
2022-02-09 11:55:05 +0530

    First change

--- Day1-7/Days_left.py
+++ Day1-7/Days_left.py
@@ -1,6 +1,7 @@
 if __name__=="__main__":
-    age = int(input("Enter your current age: "))
+    age = float(input("Enter your current age: "))
     left = 90-age
     print("Number of months left are: ",(left*12))
     print("Number of weeks left are: ",(left*52))
     print("Number of days left are: ",(left*52*7))
+    print("You must judiciously use your time!")
